c3bc03cbf5afb419f765bd87616afd6690cd08e3
lucas-carva <131870569+lucas-carva@users.noreply.github.com>
2024-11-12 22:30:52 -0300

    more exercises

---
+++ ESTRUTURAS E BIBLIOTECAS/BEE1023.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+ //incompleto
+
+int main() {
+ 
+    int cidade;
+    int moradores[50], consumo[250];
+    
+    for (int j=1;cidade=0 ;j++){
+        scanf("%d", &cidade);
+        //if(cidade==0) break;
+            for(int i=1; i<=cidade; i++){
+                scanf("%d %d", &moradores[i], &consumo[i]);
+            }
+        
+        
+        
+    }
+ 
+    return 0;
+}
